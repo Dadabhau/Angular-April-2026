fce096a5ecc5ef6bcf9ce65d8304e3c732d1a83c
Dadabhau <dadasthete987@gmail.com>
2026-04-16 19:46:22 +0530

    User Images and dummy data added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './components/header/header';
+import { User } from './components/user/user';
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
-  imports: [Header],
+  imports: [Header, User],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('Angular-April-2026');
+  users = DUMMY_USERS;
 }
