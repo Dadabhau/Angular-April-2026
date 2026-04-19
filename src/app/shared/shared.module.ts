3da644c448b1b9d7d2079943aa524d272dfa6633
Dadabhau <dadasthete987@gmail.com>
2026-04-19 12:56:24 +0530

    NG Module added

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { Card } from './components/card/card';
+
+@NgModule({
+  declarations: [Card],
+  imports: [],
+  exports: [Card],
+})
+export class SharedModule {}
